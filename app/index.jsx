d62fc77feca8a983d949df7741b5a46ff156f19b
Ahmed Missouri <ahmedmissouri@icloud.com>
2024-08-18 20:58:48 +0100

    onboarding setup

--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
-import { Link } from 'expo-router';
+import { ScrollView, Text, View, Image } from 'react-native';
+import { Redirect, router } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { images } from '../constants'
+import CustomButton from '../components/CustomButton';
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className='text-3xl font-pblack'>Pixl</Text>
-      <StatusBar style="auto" />
-      <Link href="/home" style={{color: 'blue'}}>Go to Home</Link>
-    </View>
+    <SafeAreaView className='bg-primary h-full'>
+      <ScrollView contentContainerStyle={{height: '100%'}}>
+        <View className='w-full h-[85vh] justify-center items-center px-4'>
+          {/* <Image source={images.logo} className='w-[130px] h-[84px]' resizeMode='contain' /> */}
+          <Text className='text-white text-4xl font-bold'>Pixl</Text>
+          <Image source={images.cards} className='max-w-[380px] w-full h-[300px]' resizeMode='contain' />
+
+          <View className='relative mt-5'>
+            <Text className='text-3xl text-white font-bold text-center max-w-[75%]'>
+              Express yourself with{' '}
+              <Text className='text-secondary-200 underline decoration-[#24a7cd]'>Pixl</Text>
+            </Text>
+            {/* <Image source={images.path} className='w-[75px] absolute -bottom-[6px] -right-3 h-[15px] ' resizeMode='contain' /> */}
+          </View>
+
+          <CustomButton title='Continue to Login' handlePress={() => {router.push("/sign-in")}} containerStyles='w-full mt-7' />
+        </View>
+      </ScrollView>
+
+      <StatusBar backgroundColor='#161622' style='light' />
+    </SafeAreaView>
   );
 }
